18e6b1c307fea79b217a381679ab0553dd450588
2sseung1 <103083730+SanaiJaemin@users.noreply.github.com>
2022-08-25 17:56:53 +0900

    fix : Title 장면만듬

---
+++ Assets/Scripts/Untill/CameraHit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraHit : MonoBehaviour
+{
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void OnclickEnter()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("입장");
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("나가기");
+    }
+   
+}
